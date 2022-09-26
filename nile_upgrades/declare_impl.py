7a6b21d52448203293847d135b0176f6d9ca7a1e
Eric Lau <ericglau@outlook.com>
2022-09-26 16:39:33 -0400

    Update get_hash

--- nile_upgrades/declare_impl.py
+++ nile_upgrades/declare_impl.py
@@ -2,6 +2,8 @@ import click
 
 from nile.deployments import class_hash_exists
 
+from nile.common import ABIS_DIRECTORY, BUILD_DIRECTORY
+
 from starkware.crypto.signature.fast_pedersen_hash import pedersen_hash
 from starkware.starknet.core.os.class_hash import compute_class_hash
 from starkware.starknet.services.api.contract_class import ContractClass
@@ -22,11 +24,12 @@ def declare_impl(nre, contract_name):
 
     return hash
 
-def get_hash(contract_name):
-    with open(f"artifacts/{contract_name}.json", "r") as fp:
+def get_hash(contract_name, overriding_path=None):
+    """Return the class hash (in hex) of a given contract class."""
+    base_path = (
+        overriding_path if overriding_path else (BUILD_DIRECTORY, ABIS_DIRECTORY)
+    )
+    with open(f"{base_path[0]}/{contract_name}.json", "r") as fp:
         contract_class = ContractClass.loads(fp.read())
 
-    return hex(compute_class_hash(
-        contract_class=contract_class,
-        hash_func=pedersen_hash
-    ))
+    return hex(compute_class_hash(contract_class=contract_class, hash_func=pedersen_hash))
