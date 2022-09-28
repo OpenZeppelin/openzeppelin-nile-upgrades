71511f2f20526a88b58c71a598d07578bd243aab
Eric Lau <ericglau@outlook.com>
2022-09-28 15:32:16 -0400

    Support passing in proxy alias for upgrade

--- nile_upgrades/upgrade_proxy.py
+++ nile_upgrades/upgrade_proxy.py
@@ -8,20 +8,29 @@ from nile_upgrades import declare_impl
 
 @click.command()
 @click.argument("signer", type=str)
-@click.argument("proxy_address", type=str)
+@click.argument("proxy_identifier", type=str)
 @click.argument("contract_name", type=str)
 @click.option("--max_fee", nargs=1)
 @click.option("--alias", nargs=1)
-def upgrade_proxy(proxy_address, contract_name, signer, max_fee=None, alias=None):
+def upgrade_proxy(proxy_identifier, contract_name, signer, max_fee=None, alias=None):
     """
     Upgrade a proxy to a different implementation contract.
     """
 
     nre = NileRuntimeEnvironment()
 
+    ids = deployments.load(proxy_identifier, nre.network)
+    id = next(ids, None)
+    if id is None:
+        raise Exception(f"Deployment with address or alias {proxy_identifier} not found")
+    if next(ids, None) is not None:
+        raise Exception(f"Multiple deployments found with address or alias {proxy_identifier}")
+
+    proxy_address = id[0]
+
     hash = declare_impl.declare_impl(nre, contract_name, signer, max_fee)
 
-    click.echo(f"Upgrading proxy...")
+    click.echo(f"Upgrading proxy {proxy_address}...")
     account = Account(signer, nre.network)
     account.send(proxy_address, "upgrade", calldata=[int(hash, 16)], max_fee=max_fee)
     click.echo(f"Proxy upgraded to implementation with hash {hash}")
