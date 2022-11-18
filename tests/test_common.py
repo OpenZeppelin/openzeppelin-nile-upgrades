e54fbc37fbb4ec8e69da4b72c2f17887d3d2121c
Eric Lau <ericglau@outlook.com>
2022-11-18 18:09:40 -0500

    Update tests/test_common.py
    Co-authored-by: Andrew Fleming <fleming-andrew@protonmail.com>

--- tests/test_common.py
+++ tests/test_common.py
@@ -24,7 +24,7 @@ async def test_declare_impl_already_exists(
 ):
     logging.getLogger().setLevel(logging.DEBUG)
 
-    result = await declare_impl(NETWORK, CONTRACT, SIGNER, None);
+    result = await declare_impl(NETWORK, CONTRACT, SIGNER, None)
     assert result == CLASS_HASH
 
     # check logs
