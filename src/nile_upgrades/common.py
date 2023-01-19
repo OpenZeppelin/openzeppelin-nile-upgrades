6deb2fc630dfa0e345e89af12570823ff9861c5d
Eric Lau <ericglau@outlook.com>
2023-01-19 17:47:16 -0500

    Test declare rejection

--- src/nile_upgrades/common.py
+++ src/nile_upgrades/common.py
@@ -20,13 +20,13 @@ async def declare_class(network, contract_name, account, max_fee, overriding_pat
         tx = await account.declare(contract_name, max_fee=max_fee, overriding_path=overriding_path)
         tx_status, declared_hash = await tx.execute(watch_mode="track")
 
+        if tx_status.status.is_rejected:
+            raise Exception(f"Could not declare contract class. Transaction rejected.", tx_status.error_message)
+
         if padded_hash != declared_hash:
             raise Exception(
                 f"Declared hash {declared_hash} does not match expected hash {padded_hash}"
             )
-
-        if tx_status.status.is_rejected:
-            raise Exception(f"Could not declare contract class. Transaction rejected.", tx_status.error_message)
 
         logging.debug(f"Contract class declared with hash {padded_hash}")
 
--- tests/test_common.py
+++ tests/test_common.py
@@ -22,7 +22,8 @@ WRONG_HASH = "0x000000000000000000000000000000000000000000000000000000000000011"
 KEY = "TEST_KEY"
 PRIVATE_KEY = "1234"
 MOCK_TX_HASH = 1
-TX_STATUS = TransactionStatus(MOCK_TX_HASH, TxStatus.ACCEPTED_ON_L2, None)
+TX_STATUS_ACCEPTED = TransactionStatus(MOCK_TX_HASH, TxStatus.ACCEPTED_ON_L2, None)
+TX_STATUS_REJECTED = TransactionStatus(MOCK_TX_HASH, TxStatus.REJECTED, None)
 
 
 @pytest.mark.asyncio
@@ -49,7 +50,7 @@ async def test_declare_class_already_exists(
 @patch("nile.core.types.account.Account.declare", return_value=DeclareTxWrapper(None, None))
 @patch(
     "nile.core.types.tx_wrappers.DeclareTxWrapper.execute",
-    return_value=(TX_STATUS, WRONG_HASH),
+    return_value=(TX_STATUS_ACCEPTED, WRONG_HASH),
 )
 async def test_declare_class_hash_not_match(
     mock_execute, mock_declare, mock_class_hash_exists, mock_get_hash, caplog
@@ -68,7 +69,26 @@ async def test_declare_class_hash_not_match(
 @patch("nile.core.types.account.Account.declare", return_value=DeclareTxWrapper(None, None))
 @patch(
     "nile.core.types.tx_wrappers.DeclareTxWrapper.execute",
-    return_value=(TX_STATUS, PADDED_HASH),
+    return_value=(TX_STATUS_REJECTED, PADDED_HASH),
+)
+async def test_declare_class_rejected(
+    mock_execute, mock_declare, mock_class_hash_exists, mock_get_hash, caplog
+):
+    with patch.dict(os.environ, {KEY: PRIVATE_KEY}, clear=False):
+        account = await MockAccount(KEY, NETWORK)
+
+    with pytest.raises(Exception) as e:
+        await declare_class(NETWORK, CONTRACT, account, None)
+    assert f"Could not declare contract class. Transaction rejected." in str(e.value)
+
+
+@pytest.mark.asyncio
+@patch("nile_upgrades.common.get_class_hash", return_value=CLASS_HASH)
+@patch("nile_upgrades.common.class_hash_exists", return_value=False)
+@patch("nile.core.types.account.Account.declare", return_value=DeclareTxWrapper(None, None))
+@patch(
+    "nile.core.types.tx_wrappers.DeclareTxWrapper.execute",
+    return_value=(TX_STATUS_ACCEPTED, PADDED_HASH),
 )
 async def test_declare_class(
     mock_execute, mock_declare, mock_class_hash_exists, mock_get_hash, caplog
