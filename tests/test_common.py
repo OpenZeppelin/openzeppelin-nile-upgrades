a935dd52f6bec43a88b49edb537626ef4d316e42
Eric Lau <ericglau@outlook.com>
2023-01-19 10:42:12 -0500

    Fix test_common

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,62 +1,88 @@
 """Tests for common."""
 import logging
+import os
 from unittest.mock import patch
 
 import pytest
 
+from nile.utils.status import TransactionStatus, TxStatus
+
 from nile_upgrades.common import declare_contract, get_contract_abi
+from mocks.mock_account import MockAccount
+from nile.core.types.tx_wrappers import DeclareTxWrapper
 
 
 NETWORK = "localhost"
 CONTRACT = "contract"
-SIGNER = "PKEY1"
 
 CLASS_HASH = 16
 PADDED_HASH = "0x000000000000000000000000000000000000000000000000000000000000010"
 WRONG_HASH = "0x000000000000000000000000000000000000000000000000000000000000011"
 
+KEY = "TEST_KEY"
+PRIVATE_KEY = "1234"
+MOCK_TX_HASH = 1
+TX_STATUS = TransactionStatus(MOCK_TX_HASH, TxStatus.ACCEPTED_ON_L2, None)
+
 
 @pytest.mark.asyncio
 @patch("nile_upgrades.common.get_class_hash", return_value=CLASS_HASH)
 @patch("nile_upgrades.common.class_hash_exists", return_value=True)
-async def test_declare_impl_already_exists(
+async def test_declare_class_already_exists(
     mock_class_hash_exists, mock_get_hash, caplog
 ):
     logging.getLogger().setLevel(logging.DEBUG)
 
-    result = await declare_contract(NETWORK, CONTRACT, SIGNER, None)
+    with patch.dict(os.environ, {KEY: PRIVATE_KEY}, clear=False):
+        account = await MockAccount(KEY, NETWORK)
+
+    result = await declare_contract(NETWORK, CONTRACT, account, None)
     assert result == CLASS_HASH
 
     # check logs
-    assert f"Implementation with hash {PADDED_HASH} already exists" in caplog.text
+    assert f"Contract with hash {PADDED_HASH} already exists" in caplog.text
 
 
 @pytest.mark.asyncio
 @patch("nile_upgrades.common.get_class_hash", return_value=CLASS_HASH)
 @patch("nile_upgrades.common.class_hash_exists", return_value=False)
-@patch("nile.core.account.Account.declare", return_value=WRONG_HASH)
-async def test_declare_impl_not_match(
-    mock_declare, mock_class_hash_exists, mock_get_hash, caplog
+@patch("nile.core.types.account.Account.declare", return_value=DeclareTxWrapper(None, None))
+@patch(
+    "nile.core.types.tx_wrappers.DeclareTxWrapper.execute",
+    return_value=(TX_STATUS, WRONG_HASH),
+)
+async def test_declare_class_hash_not_match(
+    mock_execute, mock_declare, mock_class_hash_exists, mock_get_hash, caplog
 ):
+    with patch.dict(os.environ, {KEY: PRIVATE_KEY}, clear=False):
+        account = await MockAccount(KEY, NETWORK)
+
     with pytest.raises(Exception) as e:
-        await declare_contract(NETWORK, CONTRACT, SIGNER, None)
+        await declare_contract(NETWORK, CONTRACT, account, None)
     assert f"Declared hash {WRONG_HASH} does not match expected hash {PADDED_HASH}" in str(e.value)
 
 
 @pytest.mark.asyncio
 @patch("nile_upgrades.common.get_class_hash", return_value=CLASS_HASH)
 @patch("nile_upgrades.common.class_hash_exists", return_value=False)
-@patch("nile.core.account.Account.declare", return_value=PADDED_HASH)
-async def test_declare_impl(
-    mock_declare, mock_class_hash_exists, mock_get_hash, caplog
+@patch("nile.core.types.account.Account.declare", return_value=DeclareTxWrapper(None, None))
+@patch(
+    "nile.core.types.tx_wrappers.DeclareTxWrapper.execute",
+    return_value=(TX_STATUS, PADDED_HASH),
+)
+async def test_declare_class(
+    mock_execute, mock_declare, mock_class_hash_exists, mock_get_hash, caplog
 ):
     logging.getLogger().setLevel(logging.DEBUG)
 
-    result = await declare_contract(NETWORK, CONTRACT, SIGNER, None)
+    with patch.dict(os.environ, {KEY: PRIVATE_KEY}, clear=False):
+        account = await MockAccount(KEY, NETWORK)
+
+    result = await declare_contract(NETWORK, CONTRACT, account, None)
     assert result == CLASS_HASH
 
     # check logs
-    assert f"Implementation declared with hash {PADDED_HASH}" in caplog.text
+    assert f"Contract declared with hash {PADDED_HASH}" in caplog.text
 
 
 def test_get_contract_abi():
